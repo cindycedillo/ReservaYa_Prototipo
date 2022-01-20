b52106ad1840557f966192a0d024a72a33678516
cindycedillo <cindy.cedillo.est@tecazuay.edu.ec>
2022-01-19 19:26:03 -0500

    Pantalla al abrir la aplicación creada

--- app/src/main/java/com/reservaya/reservaya_prototipo/MainActivity.java
+++ app/src/main/java/com/reservaya/reservaya_prototipo/MainActivity.java
@@ -3,12 +3,36 @@ package com.reservaya.reservaya_prototipo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //Se agregan las animaciones
+        Animation animacion1= AnimationUtils.loadAnimation( this,R.anim.desplazamiento_arriba);
+        Animation animacion2= AnimationUtils.loadAnimation( this,R.anim.desplazamiento_abajo);
+
+        ImageView logoIVDibujo =findViewById(R.id.logoIVDibujo);
+        ImageView logoIVnombre = findViewById(R.id.logoIVNombre);
+
+        logoIVDibujo.setAnimation(animacion1);
+        logoIVnombre.setAnimation(animacion2);
+
+        //new Handler().postDelayed(new Runnable() {
+        //@Override
+        //public void run() {
+        //   Intent intent= new Intent(MainActivity.this, SplashScreenRY.class);
+        //   startActivity(intent);
+        //   finish();
+        //}
+        //}, 4000);
     }
 }
