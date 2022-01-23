162cb2c77fe28c9a3005502897230c701f2fde7c
Jey <89442351+MayraPenafiel@users.noreply.github.com>
2022-01-23 18:28:48 -0500

    Conexion menu principal y mapa

--- app/src/main/java/com/reservaya/reservaya_prototipo/activity_menuPrincipal.java
+++ app/src/main/java/com/reservaya/reservaya_prototipo/activity_menuPrincipal.java
@@ -1,14 +1,28 @@
 package com.reservaya.reservaya_prototipo;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class activity_menuPrincipal extends AppCompatActivity {
+
+    private TextView mapa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        mapa=findViewById(R.id.btaceptarmp);
+        mapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(activity_menuPrincipal.this,Activity_mapa.class));
+            }
+        });
     }
 }
