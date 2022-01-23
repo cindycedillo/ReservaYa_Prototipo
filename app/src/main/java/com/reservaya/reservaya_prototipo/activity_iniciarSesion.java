7d93da7ea9390331f11b79e29d7c32ea63bd84d1
Jey <89442351+MayraPenafiel@users.noreply.github.com>
2022-01-23 18:00:29 -0500

    Conexion con menu principal
    Conexion del la pantalla de insstrucciones con el menu principal

--- app/src/main/java/com/reservaya/reservaya_prototipo/activity_iniciarSesion.java
+++ app/src/main/java/com/reservaya/reservaya_prototipo/activity_iniciarSesion.java
@@ -33,4 +33,5 @@ public class activity_iniciarSesion extends AppCompatActivity {
             }
         });
     }
+
 }
--- app/src/main/java/com/reservaya/reservaya_prototipo/activity_instrucciones.java
+++ app/src/main/java/com/reservaya/reservaya_prototipo/activity_instrucciones.java
@@ -2,10 +2,16 @@ package com.reservaya.reservaya_prototipo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class activity_instrucciones extends AppCompatActivity {
+
+    private TextView menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +20,18 @@ public class activity_instrucciones extends AppCompatActivity {
 
         DisplayMetrics medidas= new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(medidas);
-
         int ancho=medidas.widthPixels;
         int alto= medidas.heightPixels;
-
         getWindow().setLayout((int)(ancho*0.97),(int)(alto*0.95));
+
+        menu=findViewById(R.id.btout);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(activity_instrucciones.this,activity_menuPrincipal.class));
+            }
+        });
+
     }
+
 }
