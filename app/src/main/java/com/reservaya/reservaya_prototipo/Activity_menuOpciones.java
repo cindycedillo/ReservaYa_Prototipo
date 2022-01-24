c48d48af03a7c4c05990d14d31e23a7ab4bdd0dd
cindycedillo <cindy.cedillo.est@tecazuay.edu.ec>
2022-01-23 19:32:59 -0500

    Conexión entre menú de opciones y el menú de reservas

--- app/src/main/java/com/reservaya/reservaya_prototipo/Activity_menuOpciones.java
+++ app/src/main/java/com/reservaya/reservaya_prototipo/Activity_menuOpciones.java
@@ -5,16 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Activity_menuOpciones extends AppCompatActivity {
 
     private TextView menuopciones;
+    Button btn_reservas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_opciones);
+
+        btn_reservas=findViewById(R.id.btnreservas);
+        btn_reservas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Activity_menuOpciones.this,MenuReservas.class));
+            }
+        });
 
         menuopciones=findViewById(R.id.btinicio);
         menuopciones.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/reservaya/reservaya_prototipo/ComprobanteReserva.java
+++ app/src/main/java/com/reservaya/reservaya_prototipo/ComprobanteReserva.java
@@ -2,13 +2,26 @@ package com.reservaya.reservaya_prototipo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ComprobanteReserva extends AppCompatActivity {
+
+    Button btn_aceptar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comprobante_reserva);
+        btn_aceptar=findViewById(R.id.btnAceptar);
+        btn_aceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(ComprobanteReserva.this,Activity_menuOpciones.class);
+                startActivity(i);
+            }
+        });
     }
 }
