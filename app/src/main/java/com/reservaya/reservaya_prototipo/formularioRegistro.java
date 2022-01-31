2a032a38492820a04d972f3c6c7c9ab37901d3c1
Jey <89442351+MayraPenafiel@users.noreply.github.com>
2022-01-30 23:19:43 -0500

    correcion conexion
    Co-Authored-By: Geovanny Cuzco <89951586+MarcoCuzco@users.noreply.github.com>

--- app/src/main/java/com/reservaya/reservaya_prototipo/formularioRegistro.java
+++ app/src/main/java/com/reservaya/reservaya_prototipo/formularioRegistro.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,6 +15,12 @@ public class formularioRegistro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_registro);
+        
+        DisplayMetrics medidas= new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(medidas);
+        int ancho=medidas.widthPixels;
+        int alto= medidas.heightPixels;
+        getWindow().setLayout((int)(ancho*0.97),(int)(alto*0.95));
 
         mp.findViewById(R.id.btregistrar);
         mp.setOnClickListener(new View.OnClickListener() {
